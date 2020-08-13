1a18822082c64f4027ae6713621bc5de9643b89a
Cristhian Curiñaupa Huaman <cristhian_666win@hotmail.com>
2020-08-13 01:00:55 -0500

    Add project files.

---
+++ SolutionCore/Infrastructure/Data/CQS/Usuario/Query/IUsuarioQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolutionCore.CQR.Query.Usuario
+{
+  public  interface IUsuarioQuery
+    {
+    }
+}
---
+++ SolutionCore/Infrastructure/Data/CQS/Usuario/Query/UsuarioQuery.cs
@@ -0,0 +1,35 @@
+﻿using SolutionCore.Infrastructure.Data.Context;
+using SolutionCore.Infrastructure.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SolutionCore.CQR.Query.Usuario
+{
+    public class UsuarioQuery
+    {
+       
+        private  CoreContext _CoreContext;
+
+        public UsuarioQuery(CoreContext  CoreContext)
+        {
+            _CoreContext = CoreContext;
+        }
+
+        public IQueryable GetUsuario( ) {
+
+           var  _usuario = (from u in _CoreContext.Usuarios
+                           
+                           select new {
+                               u.UsuarioId,
+                               u.NombreCompleto
+                           });
+                           
+             return _usuario;
+        }
+        
+      
+    }
+}
