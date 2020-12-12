e58d790f12c93adaf71406c704eaafde1be9f98e
Cristhian Curiñaupa Huaman <cristhian_666win@hotmail.com>
2020-12-11 20:07:26 -0500

    Actualizacion en capas Proyecto Core

---
+++ Data Acces/SolutionCore.Api.DataAcces/Infrastructure/Data/Extensions.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SolutionCore.Distributed_Processes.Dominio.Infrastructure.Data.Context;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolutionCore.Distributed_Processes.Dominio.Infrastructure.Data
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddDataServicesSQL(this IServiceCollection services, IConfiguration configuration)
+        {
+            var productConnection = configuration.GetConnectionString("SpartacusContext");
+            services.AddDbContext<CoreContext>(options =>
+            {
+                options.UseSqlServer(productConnection);
+            });
+           
+            return services;
+        }
+
+
+    }
+}
---
+++ SolutionCore/Infrastructure/Transport/SolutionCore.Infraestructura.Transport/Core/Authorization/Response/GetConfigurationResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolutionCore.Infrastructure.Transport.Core.Authorization.Response
+{
+    public class GetConfigurationResponse
+    {
+
+        public string SecurityUrl { get; set; }
+        public string ImageUrl { get; set; }
+        public string MaintenanceUrl { get; set; }
+        public string PlanningUrl { get; set; }
+        public string TaskUrl { get; set; }
+        public string IntegrationUrl { get; set; }
+        public string SystemConfigurationUrl { get; set; }
+        public string InputUrl { get; set; }
+        public string LoggingProcessUrl { get; set; }
+
+    }
+}
