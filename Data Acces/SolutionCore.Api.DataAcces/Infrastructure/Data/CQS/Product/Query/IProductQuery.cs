ef0d800bf6ecdb8a80136e2888e9e142524ca909
Cristhian Curiñaupa Huaman <cristhian_666win@hotmail.com>
2020-12-12 22:26:09 -0500

    nuevas estructura Product

---
+++ Data Acces/SolutionCore.Api.DataAcces/Infrastructure/Data/CQS/Product/Query/IProductQuery.cs
@@ -0,0 +1,16 @@
+﻿using SolutionCore.Infrastructure.Transport.Core.Authorization.CQS.Query.Parameter;
+using SolutionCore.Infrastructure.Transport.Core.Authorization.CQS.Query.Result;
+using SolutionCore.Infrastructure.Transport.Core.Authorization.Request;
+using SolutionCore.Infrastructure.Transport.Core.Authorization.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolutionCore.Infrastructure.Data.CQS.Authorization.Query
+{
+  public  interface IProductQuery
+    {
+          
+    }
+}
