e55171c2e7dbbaf3c66cbb76aab49b2b36bf95a3
Cristhian Curiñaupa Huaman <cristhian_666win@hotmail.com>
2021-04-24 18:06:44 -0500

    docker exitoso

--- SolutionCore/Program.cs
+++ SolutionCore/Program.cs
@@ -16,40 +16,40 @@ namespace SolutionCore
 {
     public class Program
     {
-        //public static void Main(string[] args)
-        //{
-        //     CreateHostBuilder(args).Build().Run();
-
-
-        //}
-
-        public async static Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
+            CreateHostBuilder(args).Build().Run();
 
-            using var scope = host.Services.CreateScope();
-            var services = scope.ServiceProvider;
 
-            try
-            {
-                var dbContext = services.GetRequiredService<CoreContext>();
-
-                if (dbContext.Database.IsSqlServer())
-                {
-                    dbContext.Database.Migrate();
-                }
-            }
-            catch (Exception ex)
-            {
-                var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-
-                logger.LogError(ex, "An error occurred while migrating or seeding the database.");
-
-                throw;
-            }
-
-            await host.RunAsync();
         }
+
+        //public async static Task Main(string[] args)
+        //{
+        //    var host = CreateHostBuilder(args).Build();
+
+        //    using var scope = host.Services.CreateScope();
+        //    var services = scope.ServiceProvider;
+
+        //    try
+        //    {
+        //        var dbContext = services.GetRequiredService<CoreContext>();
+
+        //        if (dbContext.Database.IsSqlServer())
+        //        {
+        //            dbContext.Database.Migrate();
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+        //        logger.LogError(ex, "An error occurred while migrating or seeding the database.");
+
+        //        throw;
+        //    }
+
+        //    await host.RunAsync();
+        //}
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
