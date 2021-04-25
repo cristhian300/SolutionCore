2f40af3fc4e5ba8d5df0305befc11692f5416e4b
Cristhian Curiñaupa Huaman <cristhian_666win@hotmail.com>
2021-04-25 10:18:35 -0500

    archivos para ambientes

--- SolutionCore/Program.cs
+++ SolutionCore/Program.cs
@@ -53,10 +53,27 @@ namespace SolutionCore
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
+
+ 
+             .ConfigureAppConfiguration((hostingContext, config) =>
+             {
+                 config
+                     .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                     .AddJsonFile("appsettings.json", true, true)
+                     .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true,
+                         true)
+                     
+                     .AddEnvironmentVariables();
+             })
+
+
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
             });
+
+       
+
     }
 
 
