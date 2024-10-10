0677a7531de868e3fa04aee6c7afe333cc93b8e8
cristhian600 <ccurinaupah@bcds.net>
2024-10-09 23:00:46 -0500

    ambiente

--- web_core/ClientApp/src/app/main/providers/services/Product/product.service.ts
+++ web_core/ClientApp/src/app/main/providers/services/Product/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { NetworkManager } from 'src/app/main/providers/legacy-agent/network-manager';
 import { PostParameter } from 'src/app/main/providers/legacy-agent/post-parameter';
-import { environment } from 'src/environments/environment.prod';
+
 
 import { StorageService } from '../common/storage.service';
 import { ConfigurationResponse } from '../configuration/configuration';
@@ -10,6 +10,7 @@ import { AddProductRequest, EditProductRequest   } from './product.interface';
 import { ApiService } from 'src/core/shared/common/services/services/api/api.service';
 import { ResponseDTO } from 'src/app/main/models/DTOs/response-dto';
 import { ListProductResponse } from 'src/app/main/models/DTOs/Product/productosDto';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
