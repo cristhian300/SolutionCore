53881a3740bcdc7a9abf5244af9aa9fa91779384
cristhian600 <ccurinaupah@bcds.net>
2024-10-10 15:06:55 -0500

    puerto pro

--- web_core/ClientApp/src/environments/environment.prod.ts
+++ web_core/ClientApp/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiEndpoint: "http://ec2-3-142-95-144.us-east-2.compute.amazonaws.com:700/",
+  apiEndpoint: "http://ec2-3-142-95-144.us-east-2.compute.amazonaws.com:83",
+
   apiEndpointNode: 'http://localhost:3001'
 };
