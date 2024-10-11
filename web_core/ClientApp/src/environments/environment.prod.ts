cd271b3b8acce53f2a4f88dc2821af5648b0b18f
cristhian600 <ccurinaupah@bcds.net>
2024-10-10 23:51:35 -0500

    actulizacion service

--- web_core/ClientApp/src/environments/environment.prod.ts
+++ web_core/ClientApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiEndpoint: "http://ec2-3-142-95-144.us-east-2.compute.amazonaws.com:83",
+  apiEndpoint: "http://3.146.135.57:83",
 
   apiEndpointNode: 'http://localhost:3001'
 };
